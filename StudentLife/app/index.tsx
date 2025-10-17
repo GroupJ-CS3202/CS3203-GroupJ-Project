d305ede9640037a4477d299781e3b94928592749
dyeaman05 <danielyeaman05@gmail.com>
2025-10-17 10:58:32 -0500

    git branchgit branchgit branchgit branchgit branchgit branch! test

--- StudentLife/app/index.tsx
+++ StudentLife/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-    <Text>Welcome to Student Life!! :D</Text>
+    <Text>Welcome to Student Life!!! :D</Text>
     </View>
   );
 }
