73e094b4d308a036d13ac7c5b0e3160fd65e6e80
Caesar1323 <caydensgt@gmail.com>
2025-12-11 16:58:53 -0600

    authentication service fixes- sabrin

--- StudentLife/services/authService.ts
+++ StudentLife/services/authService.ts
@@ -45,6 +45,64 @@ export async function login (email : string, password : string): Promise<LoginRe
     }
 }
 
+//If the user successfully logs in then we will attempt to save the token to the browser's local storage
 export function saveAuth(token:string, user : User)
 {
+    try 
+    {
+        localStorage.setItem(TOKEN_KEY, token);
+        localStorage.setItem(USER_KEY, JSON.stringify(user));
+    }
+    catch 
+    {
+
+    }
+}
+
+//attempts to read the user's saved authentication token form local storage on app startup
+
+export function loadAuth(): LoginResult | null 
+{
+    try
+    {
+        const token = localStorage.getItem(TOKEN_KEY);
+        const rawUser = localStorage.getItem(USER_KEY);
+        if (!token || !rawUser) return null;
+
+        const user = JSON.parse(rawUser) as User;
+
+        return {token, user};
+    }
+    catch
+    {
+        return null; //return null if there are any issues attempting to get token and user info
+    }
+
+}
+
+//To be used during any logout protocol
+export function clearAuth() 
+{
+    try 
+    {
+        localStorage.removeItem(TOKEN_KEY);
+        localStorage.removeItem(USER_KEY);
+    }
+    catch 
+    {
+        //ignore for now
+    }
+}
+
+export function getAuthToken () : string | null 
+{
+    try 
+    {
+        return localStorage.getItem(TOKEN_KEY);
+
+    }
+    catch 
+    {
+        return null;
+    }
 }
